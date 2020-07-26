e97216a21a01d9af21a5160d3b6578266c5a56c8
Marko Krstevski <marsy.krstevski@gmail.com>
2020-07-26 22:03:49 +0200

    first commit - initialize

---
+++ src/main/java/com/finki/pizzaslice/PizzasliceApplication.java
@@ -0,0 +1,13 @@
+package com.finki.pizzaslice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzasliceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzasliceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/finki/pizzaslice/PizzasliceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finki.pizzaslice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzasliceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
